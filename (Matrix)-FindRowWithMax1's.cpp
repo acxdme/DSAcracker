8513d1c4a0e26374d57e42351011d947ecf20edc
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-12-02 15:33:19 +0530

    Create (Matrix)-FindRowWithMax1's.cpp

---
+++ (Matrix)-FindRowWithMax1's.cpp
@@ -0,0 +1,7 @@
+/*
+Time comlexity :
+space complexity :
+My approach :  The naive way is to count in each row the number of 1's , this would take O(N*M) time , the naive appraoch  does not take advantage of the fact that the 
+              matrix is row wise sorted.
+*/
+
